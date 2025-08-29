2c1df3b1959d9ed52212ee45a009e44ffd11ce9e
nkx149 <147606630+nkx149@users.noreply.github.com>
2025-08-29 15:50:14 +0800

    Add files via upload

---
+++ Models/PageModel.cs
@@ -0,0 +1,18 @@
+﻿namespace PdfWebApp.Models
+{
+    public class PageModel
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public int PageNumber { get; set; }
+        
+        public required string ThumbnailImgUrl { get; set; }
+
+        public string? PreviewImgUrl { get; set; }
+
+        public required string Zone {  get; set; }
+
+        public byte[]? PdfPageBytes { get; set; }
+
+        public string? OriginalLabel { get; set; }
+    }
+}
---
+++ Models/UndoStackItem.cs
@@ -0,0 +1,11 @@
+﻿namespace PdfWebApp.Models
+{
+    public class UndoStackItem
+    {
+        public PageModel? Page { get; set; }
+
+        public int MainIndex { get; set; }
+
+        public int ZoneIndex { get; set; }
+    }
+}
